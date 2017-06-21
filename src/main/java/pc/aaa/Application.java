a8916e69f3563c11d140824baba0f0126daa811e
zzzccca <a820714>
2017-06-21 10:41:51 +0800

    代码的首次提交 初步完善小程序登录及word的简单下载功能

--- src/main/java/pc/aaa/Application.java
+++ src/main/java/pc/aaa/Application.java
@@ -3,6 +3,7 @@ package pc.aaa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -14,8 +15,12 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
-
 }
